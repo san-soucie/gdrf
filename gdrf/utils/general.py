0ebec788c31d67036615dc2000af21ba75af45a0
John San Soucie <jsansoucie@whoi.edu>
2021-10-05 21:37:12 -0400

    removing gpu log string

--- gdrf/utils/general.py
+++ gdrf/utils/general.py
@@ -481,16 +481,16 @@ def select_device(device=""):
         #     s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2}MB)\n"  # bytes to MB
         try:
             gpu_number = os.environ["CUDA_VISIBLE_DEVICES"].split(",")[0]
-            p = torch.cuda.get_device_properties(f"cuda:{gpu_number}")
-            s += f"CUDA:{gpu_number} ({p.name}, {p.total_memory / 1024 ** 2}MB)\n"
+            # p = torch.cuda.get_device_properties(f"cuda:{gpu_number}")
+            # s += f"CUDA:{gpu_number} ({p.name}, {p.total_memory / 1024 ** 2}MB)\n"
         except KeyError:
             pass
     else:
         s += "CPU\n"
 
-    LOGGER.info(
-        s.encode().decode("ascii", "ignore") if platform.system() == "Windows" else s
-    )  # emoji-safe
+    # LOGGER.info(
+    #     s.encode().decode("ascii", "ignore") if platform.system() == "Windows" else s
+    # )  # emoji-safe
     return torch.device(f"cuda:{gpu_number}" if cuda else "cpu")
 
 
